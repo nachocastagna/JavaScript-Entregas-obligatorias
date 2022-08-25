62b71a1e052ad5902b6d3f517eb0716078f3dedb
nachocastagna <nacho_castagna@hotmail.com>
2022-08-25 18:42:50 -0300

    4 Casi finalizado

--- 4.Simulador Interactivo/index.js
+++ 4.Simulador Interactivo/index.js
@@ -1,111 +1,65 @@
-//CALCULADORA
+//Todo en porncentaje
 
-/* let pregunta = prompt("¿Quieres realizar alguna operacion?");
-operaciones = ["sumar", "restar", "multiplicar", "dividir"];
-
-while(pregunta.toLowerCase() == "si"){
-
-    respuesta = prompt("¿Que operacion quieres realizar? --> Sumar - Restar - Dividir - Multiplicar");
-    
-    if(respuesta.toLowerCase() === operaciones[0]){
-        
-        alert("Ok, vamos a SUMAR 2 numeros");
-        num1 = parseFloat(prompt("Elige el primer numero"))  ;
-        num2 = parseFloat(prompt("Elige el segundo numero"));
-        resultado = num1 + num2;
-        mensaje = (`${num1} + ${num2} = ${resultado}`);
-        alert(mensaje);
-        break;
-    
-    }
-    else if(respuesta.toLowerCase() == operaciones[1]){
-        alert("Ok, vamos a RESTAR 2 numeros");
-        num1 = parseFloat(prompt("Elige el primer numero"))  ;
-        num2 = parseFloat(prompt("Elige el segundo numero"));
-        resultado = num1 - num2;
-        mensaje = (`${num1} - ${num2} = ${resultado}`);
-        alert(mensaje);
-        break;
-    }
-
-    else if(respuesta.toLowerCase() == operaciones[2]){
-        alert("Ok, vamos a MULTIPLICAR 2 numeros");
-        num1 = parseFloat(prompt("Elige el primer numero"))  ;
-        num2 = parseFloat(prompt("Elige el segundo numero"));
-        resultado = num1 * num2;
-        mensaje = (`${num1} * ${num2} = ${resultado}`);
-        alert(mensaje);
-        break;
-    }
-
-
-    else if(respuesta.toLowerCase() == operaciones[3]){
-        alert("Ok, vamos a DIVIDIR 2 numeros");
-        num1 = parseFloat(prompt("Elige el primer numero"))  ;
-        num2 = parseFloat(prompt("Elige el segundo numero"));
-        resultado = num1 / num2;
-        mensaje = (`${num1} / ${num2} = ${resultado}`);
-        alert(mensaje);
-        break;
-    }
-
-    else{
-        break;
-    }
-}
-
-alert("Gracias por utilizar nuestra aplicacion"); */
-
-
-const ahorro = 10
+const ahorro = 10 
 const cuotas12 = 1.35
 const cuotas18 = 1.50
 const cuotas24 = 1.65
 
-
+//Funciones
 function interes (a,b){
     precioInteres = a*b;
-    return `$ ${precioInteres}`
+    return `${precioInteres}`
 }
 
 function descuento(a,b){
     precioDescuento = a-(a/b);
-    return `$ ${precioDescuento}`
+    return `${precioDescuento}`
+}
+
+function numcuotas(a,b){
+    precioCuotas = a/b;
+    return `${precioCuotas}`
 }
 
 
-let precioProducto = prompt("Ingrese el precio del producto: ");
 
-while(precioProducto >=1){
+//Simulador
+let precioProducto = parseFloa (prompt("Ingrese el precio del producto: "));
+
+while(precioProducto >0){
     pregunta = prompt("¿Va a pagar en efectivo o con tarjeta?");
-    if(pregunta.toLowerCase() != "tarjeta"){
-    alert("Tendra un 10% de descuento. Su precio final es:");
-    alert(descuento(precioProducto,ahorro));
+ 
+    if(pregunta.toLowerCase() === "efectivo"){
+    alert(`Tendra un 10% de descuento y su precio final seria: ${descuento(precioProducto,ahorro)} `);
     break;
     }
 
-
     else if(pregunta.toLowerCase() === "tarjeta"){
-    cuotas = parseInt (prompt(`En cuantas cuotas quiere abonar? \n12 --> 35% \n18 --> 50% \n24 --> 65%`))}
+    cuotas = parseInt (prompt(`En cuantas cuotas quiere abonar? \n12 --> 35% \n18 --> 50% \n24 --> 65%`));
     switch(cuotas){
         case 12:
-        let precioFinal = parseInt (alert(interes(precioProducto,cuotas12)));
+        precioFinal = parseInt (alert(interes(precioProducto,cuotas12)));
+        mensaje = alert(`${cuotas} cuotas de ${precioFinal}/${cuotas} = ${precioFinal}`)
         break;
 
         case 18:
         precioFinal = parseInt (alert(interes(precioProducto,cuotas18)))
-        
+        mensaje = alert(`${cuotas} cuotas de ${precioFinal}/${cuotas} = ${precioFinal}`)
         break;
 
         case 24:
-        precioFinal = parseInt (alert(interes(precioProducto,cuotas24)));
+        precioFinal = parseInt(alert(interes(precioProducto,cuotas24)));
+        montoDeLaCuota = parseInt(alert(`${precioFinal}/${cuotas}`))
+        mensaje = alert(`${cuotas} de ${predioDeLaCuota}`);
         break;
 
         default:
-        alert("Elija correctamente las opciones"); 
-        
+        cuotas = parseInt (prompt(`En cuantas cuotas quiere abonar? \n12 --> 35% \n18 --> 50% \n24 --> 65%`));
+        break;}
     }
-    
+
+    else{
+        break;}
 }
 
-alert("Vuelve a cargar la pagina y coloca un monto");
+alert("Vuelve a cargar la pagina con F5 y pon el precio del producto");
