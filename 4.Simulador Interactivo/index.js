ba4ce46e4108617ffcc8b3d8c5b3fe54da1e6b1a
nachocastagna <nacho_castagna@hotmail.com>
2022-08-25 19:15:46 -0300

    4.Desafio terminado

--- 4.Simulador Interactivo/index.js
+++ 4.Simulador Interactivo/index.js
@@ -1,8 +1,8 @@
 //Todo en porncentaje
 
 const ahorro = 10 
-const cuotas12 = 1.35
-const cuotas18 = 1.50
+const cuotas12 = 1.21
+const cuotas18 = 1.32
 const cuotas24 = 1.65
 
 //Funciones
@@ -23,34 +23,34 @@ function numcuotas(a,b){
 
 
 
+
 //Simulador
-let precioProducto = parseFloa (prompt("Ingrese el precio del producto: "));
+let precioProducto = parseInt (prompt("Ingrese el precio del producto: "));
 
 while(precioProducto >0){
     pregunta = prompt("¿Va a pagar en efectivo o con tarjeta?");
  
     if(pregunta.toLowerCase() === "efectivo"){
-    alert(`Tendra un 10% de descuento y su precio final seria: ${descuento(precioProducto,ahorro)} `);
+    alert(`Tendra un 10% de descuento y su precio final seria: $${descuento(precioProducto,ahorro)} `);
     break;
     }
 
     else if(pregunta.toLowerCase() === "tarjeta"){
-    cuotas = parseInt (prompt(`En cuantas cuotas quiere abonar? \n12 --> 35% \n18 --> 50% \n24 --> 65%`));
+    cuotas = parseInt (prompt(`En cuantas cuotas quiere abonar? \n12 --> ${(cuotas12*100)-100}% \n18 --> ${(cuotas18*100)-100}%  \n24 --> ${(cuotas24*100)-100}%` ));
     switch(cuotas){
         case 12:
-        precioFinal = parseInt (alert(interes(precioProducto,cuotas12)));
-        mensaje = alert(`${cuotas} cuotas de ${precioFinal}/${cuotas} = ${precioFinal}`)
+        precioFinal = parseInt((`${interes(precioProducto,cuotas12)}`));
+        mensaje = alert(`${cuotas} cuotas de $${numcuotas(precioFinal,cuotas)} = $${precioFinal}`);
         break;
 
         case 18:
-        precioFinal = parseInt (alert(interes(precioProducto,cuotas18)))
-        mensaje = alert(`${cuotas} cuotas de ${precioFinal}/${cuotas} = ${precioFinal}`)
+        precioFinal = parseInt((`${interes(precioProducto,cuotas18)}`));
+        mensaje = alert(`${cuotas} cuotas de $${numcuotas(precioFinal,cuotas)} = $${precioFinal}`);
         break;
 
         case 24:
-        precioFinal = parseInt(alert(interes(precioProducto,cuotas24)));
-        montoDeLaCuota = parseInt(alert(`${precioFinal}/${cuotas}`))
-        mensaje = alert(`${cuotas} de ${predioDeLaCuota}`);
+        precioFinal = parseInt((`${interes(precioProducto,cuotas24)}`));
+        mensaje = alert(`${cuotas} cuotas de $${numcuotas(precioFinal,cuotas)} = $${precioFinal}`);
         break;
 
         default:
